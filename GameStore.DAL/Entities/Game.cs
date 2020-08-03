e6167210c28cd31cede00eb4927dbcdf6c57e011
Vladyslav Korniienko <Vladyslav_Korniienko@epam.com>
2020-08-03 03:57:10 +0300

    Add DAL and entity 'Game'

---
+++ GameStore.DAL/Entities/Game.cs
@@ -0,0 +1,12 @@
+﻿namespace GameStore.DAL.Entities
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool IsActiveOffer { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
