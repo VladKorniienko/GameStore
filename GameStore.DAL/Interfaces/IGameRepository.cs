042d30513e236e88d9a6c80211ed874e8a737c59
Vladyslav Korniienko <Vladyslav_Korniienko@epam.com>
2020-08-06 18:03:54 +0300

    Add Update

--- GameStore.DAL/Interfaces/IGameRepository.cs
+++ GameStore.DAL/Interfaces/IGameRepository.cs
@@ -11,6 +11,7 @@ namespace GameStore.DAL.Interfaces
         Task<Game> GetAsync(int id);
         Task<Game> AddAsync(Game game);
         Task<bool> AddGameGenreAsync(int gameId, int genreId);
+        void Update (Game game);
         Task<bool> RemoveAsync(int id);
     }
 }
